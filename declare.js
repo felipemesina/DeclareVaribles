ec8bf4f47465da100cbd0ddf7b46039e2e37ac49
Felipe <felipejmesina@gmail.com>
2017-08-20 00:20:01 -0700

    created declarevaribles JS page

---
+++ declare.js
@@ -0,0 +1,26 @@
+var x= -5;
+console.log(x);
+
+var y= 17;
+console.log(y);
+
+var z= 32.6;
+console.log(z);
+
+var b= 647.896;
+console.log(b);
+
+var cheeseburger= "yes please!";
+console.log(cheeseburger);
+
+var will= "succeed!";
+console.log("I will " + will);
+
+var cd= "coding";
+var dc= "dojo";
+console.log("I <3 " + cd+" "+ dc);
+
+var success= true;
+var failure= false;
+
+y = x;
